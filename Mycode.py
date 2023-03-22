78ae552a838e3c07c4dcd94a304999d55459bf62
adithyakrishnas <adithyakrishnas2015@gmail.com>
2023-03-22 22:15:29 +0530

    input

--- Mycode.py
+++ Mycode.py
@@ -1,4 +1,17 @@
+# x = int(input("Enter 1st number"))
+# y = int(input("Enter 2nd number"))
 x = 5
-y = 8
+y = 6
 z = x + y
 print(z)
+#
+# # ch = input("enter the char")
+# # print(ch[0])
+#
+# # ch = input("enter the char ")[0]
+# # print(ch)
+#
+# result = eval(input("enter an expr "))
+# print(type(result))
+# print(result)
+
