f32142fab53a7ff88fc86926bdf19917f1d6f5e9
adithyakrishnas <adithyakrishnas2015@gmail.com>
2023-03-23 15:17:39 +0530

    break_continuw_pass

---
+++ brake_continue_pass.py
@@ -0,0 +1,30 @@
+# av = 5
+#
+# x = int(input("How many candies you want?"))
+# i = 1
+# while i <= x:
+#     if i > av:
+#         print("Out of stock")
+#         break
+#     print("candy")
+#     i += 1
+# print("Bye")
+
+# for i in range(1, 101):
+#     if i % 3 == 0 and i % 5 == 0:
+#         continue
+#     print(i)
+# print("Bye")
+
+# for i in range(1, 101):
+#     if i % 2 != 0:
+#         pass
+#     else:
+#         print(i)
+# print("Bye")
+
+for i in range(5):
+    if i == 3:
+        # continue
+        break
+    print("Hello ", i)
